4f953990bfd225cc7903a98560b88fe86ddec874
Michael <kontakt@ozum.pl>
2024-03-29 12:12:04 +0100

    style: add smooth scrolling effect to anchor links

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ contactForm.addEventListener('submit', function(event) {
     emailField.value = '';
     messageField.value = '';
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
